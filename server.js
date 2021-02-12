730d3f58944d60e1b9e76a2d946bab3ca6b19ee0
alidemirbay <alidemirbay@gmail.com>
2021-02-12 15:07:26 -0800

    adds server.js file

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3000;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// Requiring our routes
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Syncing our database and logging a message to the user upon success
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
